74235494935b5b233ae83da0a1d3cf11789b3e5c
akashpawar45 <akashgp1643pawar@gmail.com>
2024-01-04 00:44:17 +0530

    version 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import data from "../sample.json";
+import test_profile from "/jonathan.jpg";
 
 export default function Footer() {
   return (
@@ -7,21 +8,21 @@ export default function Footer() {
         <div key={i} className=" bg-slate-100 w-full h-80">
             <div className=" container m-auto h-full">
                 <div className=" w-full h-full flex flex-row justify-between align-middle gap-32">
-                    <div className="">
-                        <p className="rounded-full p-2 bg-slate-300"></p>
+                    <div className=" flex items-center">
+                        <div className="rounded-full h-8 w-8 bg-slate-300 flex justify-center items-center font-black">&lt;</div>
                     </div>
                     <div className=" flex flex-col justify-center align-middle">
                         <p className=" text-2xl font-bold text-center">"{item.testimonial.text}"</p>
-                        <div className=" flex flex-row justify-center py-4">
-                            <img className=" rounded-full" src="" alt="" />
-                            <div>
+                        <div className=" flex flex-row justify-center py-4 gap-2">
+                            <img className=" rounded-full h-10 w-10" src={test_profile} alt="" />
+                            <div className=" text-sm">
                                 <p className=" font-semibold">{item.testimonial.reviewer_name}</p>
                                 <p>{item.testimonial.reviewer_designation}</p>
                             </div>
                         </div>
                     </div>
-                    <div className=" ">
-                        <p className="rounded-full p-2 bg-slate-300"></p>
+                    <div className=" flex items-center">
+                        <p className="rounded-full h-8 w-8 bg-slate-300 flex justify-center items-center font-black">&gt;</p>
                     </div>
                 </div>
             </div>
